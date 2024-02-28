d0db9c057dd93198358e170d95574e216d254dae
Ateeb Khan <ateeb@fynder.ai>
2024-02-28 18:53:37 +0500

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 import { applicationModule } from './app.module';
 import { Logger } from './util/logger.util';
@@ -6,6 +8,11 @@ import { Logger } from './util/logger.util';
 import { APPLICATION_CONSTANTS } from './shared/constant/application.constant';
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 applicationModule(app);
 
 app.listen(APPLICATION_CONSTANTS.PORT, () => {
