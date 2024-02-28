4919ad6730cf3c84b3ce7306e0153d7f7965b1da
Ateeb Khan <ateeb@fynder.ai>
2024-02-28 18:59:54 +0500

    changes

--- src/util/env.util.ts
+++ src/util/env.util.ts
@@ -3,7 +3,9 @@ import { config } from 'dotenv';
 config();
 
 const EnvSchema = z.object({
+  NODE_ENV: z.enum(['production', 'development']).default('development'),
   PORT: z.string().default('5000'),
+  JWT_ACCESS_SECRET: z.string(),
 });
 
 export type IEnv = z.infer<typeof EnvSchema>;
@@ -11,10 +13,12 @@ export type IEnv = z.infer<typeof EnvSchema>;
 function envInitializer() {
   try {
     return EnvSchema.parse(process.env);
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
+    console.log('Env', error.errors[0].path[0], 'is', error.errors[0].message);
     process.exit(1);
   }
 }
-envInitializer();
+
 export const ENV: IEnv = envInitializer();
+export const isDev = () => ENV.NODE_ENV == 'development';
+export const isProd = () => ENV.NODE_ENV == 'production';
